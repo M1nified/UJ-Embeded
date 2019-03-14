e7edde515e0f9243d4f2338b1712e9703760b871
Michał <M1nified@users.noreply.github.com>
2019-03-14 12:37:30 +0100

    Add files via upload

---
+++ sketch_feb28a/sketch_feb28a.ino
@@ -0,0 +1,40 @@
+int RXLED = 13; 
+int i = 0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(RXLED, OUTPUT); // Set RX LED as an output
+
+  
+  Serial.begin(9600);
+  Serial.setTimeout(5000); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//  digitalWrite(RXLED, HIGH);
+//  delay(1000);
+//  digitalWrite(RXLED, LOW);
+//  delay(1000);
+
+//  Serial.write("hello\n\r");
+  String cmd = Serial.readStringUntil('\n');
+  char cmd_c[10];
+  cmd.toCharArray(cmd_c, 10);
+
+  if(cmd == "on"){
+    digitalWrite(RXLED, HIGH);
+    
+  }
+  if(cmd == "off"){
+    digitalWrite(RXLED, LOW);
+  }
+  
+  String str = String(i++);
+  char c[5];
+  str.toCharArray(c,5);
+  Serial.write(c);
+  Serial.write(cmd_c);
+  Serial.write("\n\r");
+
+}
---
+++ sketch_mar07a/sketch_mar07a.ino
@@ -0,0 +1,25 @@
+#include <LiquidCrystal.h>
+
+const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const byte ledPin = 12;
+const byte interruptPin = 2;
+volatile byte state = HIGH;
+
+void setup() {
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+}
+
+void loop() {
+  
+}
+
+void blink() {
+  state = !state;
+}
+
